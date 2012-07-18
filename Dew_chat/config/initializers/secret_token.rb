686366624f0a0d8028d8169729a88d5452a28f79
Luis Chipa <l.chipa.e@gmail.com>
2012-07-17 21:53:49 -0500

    Chat online

---
+++ Dew_chat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DewChat::Application.config.secret_token = 'f89bfd4dbfeccdadc69ea29ea353acd7593b81e065f84aeda750cafe8a98a4e91088af59d33e734546d79ad7fe36857aa09895aab8fc991e598ffd2e68656e59'
