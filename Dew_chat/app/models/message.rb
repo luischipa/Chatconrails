c810c1d2510738c8535d4bc48d5d487943160be6
c <luischipa@gmail.com>
2012-07-21 13:57:02 -0500

    completo

--- Dew_chat/app/models/message.rb
+++ Dew_chat/app/models/message.rb
@@ -1,2 +1,13 @@
 class Message < ActiveRecord::Base
+	Pusher.app_id = '24399'
+	Pusher.key = '5b6881d2b9fa5258b091'
+	Pusher.secret = '87c7830e7724b588e1f5'
+
+after_create :send_to_pusher
+
+	def send_to_pusher
+		logger.info "Hi?"
+		Pusher['DEWchat'].trigger("message:create",self.to_json)
+	end	
+
 end
